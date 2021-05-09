afd6d65b7c03c09eab6d28b6e41af149bca64c9d
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-09 15:49:57 -0400

    added github question and id replacement

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const { mainModule } = require('node:process')
 
-inquirer.prompt([{
-	type: 'input',
-	message: 'Please enter name',
-	name: 'name'
+inquirer
+	.prompt([{
+	
+		type: 'input',
+		message: 'Please enter member name',
+		name: 'name'
 },
 {
-	type: 'input',
-	message: 'Please enter id number',
-	name: 'id-num'
+		type: 'input',
+		message: 'Please enter member id number',
+		name: 'id-num'
 },
 {
-	type: 'input',
-	message: 'Please enter email',
-	name: 'email'
+		type: 'input',
+		message: 'Please enter member email',
+		name: 'email'
+},
+{
+		type: 'input',
+		message: 'Please enter member github',
+		name: 'github'
 }
 ])
