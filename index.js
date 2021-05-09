97245e17dad9e3aae6b03c43abdec332b864133f
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-09 11:01:13 -0400

    beginning question prompts

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+
+inquirer.prompt([{
+	type: 'input',
+	message: 'Please enter name',
+	name: 'name'
+},
+{
+	type: 'input',
+	message: '',
+	name: ''
+}
+
+])
