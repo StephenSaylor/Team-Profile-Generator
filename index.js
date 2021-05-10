7484bf8180c490fd1f6c7d82b61d288395d79be2
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-10 18:50:58 -0400

    adjusted role prompts for no repeat questions

--- index.js
+++ index.js
@@ -29,30 +29,38 @@ function newMember() {
 		'Engineer',
 		'Intern',
 	]
-])}
-	.then(function( {
+}
+])
+	.then(function({name, role, id, email}) {
+		let rolePrompt = ''
 		if (role === 'Engineer') {
-			inquirer.prompt([
-				{
-					type: 'input',
-					message: 'Please enter member github',
-					name: 'github',
-			},
-			}])
-		} else if (role === 'Intern') {
-			inquirer.prompt([
-				{
-					type: 'input',
-					message: 'Please enter school',
-					name: 'school',
-			}
+			rolePrompt === 'member github'
 		} else if (role === 'Manager') {
-			inquirer.prompt([
-				{	
-					type: 'input',
-					message: 'Please enter office number',
-					name: 'office',
+			rolePrompt === 'office number'
+		} else {
+			rolePrompt === 'school'
+		}
+		inquirer.prompt([
+			{
+				type: 'input',
+				message: 'Please enter member github',
+				name: 'github',
 			},
+	// 		}])
+	// 	} else if (role === 'Intern') {
+	// 		inquirer.prompt([
+	// 			{
+	// 				type: 'input',
+	// 				message: 'Please enter school',
+	// 				name: 'school',
+	// 		}
+	// 	} else if (role === 'Manager') {
+	// 		inquirer.prompt([
+	// 			{	
+	// 				type: 'input',
+	// 				message: 'Please enter office number',
+	// 				name: 'office',
+	// 		},
 
 
-	}
+	// }
