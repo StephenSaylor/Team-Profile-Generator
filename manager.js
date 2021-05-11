eb7e2348edeba28891c8bb5c6b7d9759f6374c26
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-10 20:45:29 -0400

    added require constructors on index

--- manager.js
+++ manager.js
@@ -1,6 +1,6 @@
 const Employee = require('./employee')
 
-class Intern extends Employee {
+class Manager extends Employee {
 	constructor(name, id, email, offNum) {
 		super(name, id, email)
 		this.offNum = offNum
